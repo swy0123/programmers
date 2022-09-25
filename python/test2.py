2c0545e2e53bc3f24333e17e67b927d90e4d383d
swy0123 <61828877+swy0123@users.noreply.github.com>
2022-09-25 22:30:03 +0900

    2022-09-25

---
+++ python/test2.py
@@ -0,0 +1,28 @@
+import sys
+
+answer = 0
+arr = list(map(int, input().split()))
+length = len(arr)
+print(length)
+
+
+for i in range(length):
+    cur = arr[:i+1]
+    cur.sort()
+    print(cur[i//3])
+    answer += cur[i//3]
+
+print(answer)
+print(arr)
+
+# 9 1 3 2 7 0 -2 4 5
+# [-2, 0, 1, 2, 3, 4, 5, 7, 9]
+
+# 0~8
+
+# -2
+# -2
+# -2
+# 0
+# 0
+# 0
